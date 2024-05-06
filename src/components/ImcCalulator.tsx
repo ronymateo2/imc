ca9a76ce9d7faf10558c8604ece3efd2f643f2c3
Rony Mateo <rony.mateo@ext.abcdin.cl>
2024-05-05 22:17:35 -0500

    change validations

--- src/components/ImcCalulator.tsx
+++ src/components/ImcCalulator.tsx
@@ -6,7 +6,7 @@ export default function IMCCalculator() {
   const [heigth, setHeigth] = useState<Maybe<number>>();
   const [weight, setWeight] = useState<Maybe<number>>();
 
-  const imc = heigth && weight ? weight / Math.pow(heigth, 2) : 1;
+  const imc = heigth && weight ? weight / Math.pow(heigth, 2) : undefined;
 
   return (
     <div>
@@ -14,13 +14,21 @@ export default function IMCCalculator() {
       <input
         value={heigth ?? ""}
         type="text"
-        onChange={(e) => setHeigth(Number.parseInt(e.target.value))}
+        onChange={(e) =>
+          setHeigth(
+            e.target.value ? Number.parseInt(e.target.value) : undefined
+          )
+        }
       ></input>
       WEIGHT
       <input
         value={weight ?? ""}
         type="text"
-        onChange={(e) => setWeight(Number.parseInt(e.target.value))}
+        onChange={(e) =>
+          setWeight(
+            e.target.value ? Number.parseInt(e.target.value) : undefined
+          )
+        }
       ></input>
       <div>
         <label>{imc}</label>
